95a46889271250d70bf97582dce568a4f601e028
Ethan <ethan.hawk@valpo.edu>
2021-08-08 17:11:25 -0500

    chromium: import at 92.0.4515.107 from https://github.com/kyx0r/repo-community

--- community/chromium/files/chromium.sh
+++ community/chromium/files/chromium.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-export CHROME_WRAPPER=/usr/lib/chromium/chromium
-
-CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-
 # shellcheck disable=2086
-exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@"
+exec /usr/lib/chromium/chromium \
+    --enable-gpu-rasterization \
+    $CHROME_FLAGS "$@"
